9e2a792ba56e66fd02c979b73dd7e030d9f69870
Thibault Cotel <cotelthibault@gmail.com>
2024-02-28 18:09:58 +0100

    feat: router configuration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './styles/index.css';
 import reportWebVitals from './reportWebVitals';
+import Banner from './components/Banner'
+import Footer from './components/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Error from './components/Error';
+import Home from './pages/Home'
+import Header from './components/Header'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/*' element={<Error />} />
+      </Routes>
+    </Router>
+    <Banner />
+    <Footer />
   </React.StrictMode>
 );
 
